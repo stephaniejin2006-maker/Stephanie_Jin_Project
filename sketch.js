10688589c3a2cb1600c98627ef831aeec09da834
Stephanie Jin <stephaniejin2006@gmail.com>
2025-09-16 18:26:17 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(500, 500);
+}
+
+function draw() {
+//type anything....
+background(255,0,0);
+
+//first circle
+circle(250,250,100);
+
+}
